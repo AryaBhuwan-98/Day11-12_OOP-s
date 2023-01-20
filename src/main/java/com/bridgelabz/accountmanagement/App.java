3a09f7a318f00cecdf54a2633c060489e4b3188f
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-20 10:24:49 -0800

    Add:Welcome to the Stock and Management Program

---
+++ src/main/java/com/bridgelabz/accountmanagement/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.accountmanagement;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Stock Account Management" );
+    }
+}
